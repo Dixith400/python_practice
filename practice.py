62cccbdaf4a791d68a1edad72c8662d1ed0dec72
Dixith <dixith400@gmail.com>
2025-07-25 18:13:31 +0530

    practice.py

--- practice.py
+++ practice.py
@@ -35,3 +35,34 @@ def unRepeted_sent(sentences):
 sentences=["AI is powerful", "We love coding", "AI is powerful", "Humans teach machines", "We love coding"]
 result=unRepeted_sent(sentences)
 print(result)
+
+# for frequent words 
+def frequent_Words(sentences , stop_words):
+    count={}
+    for n in sentences:
+        words = n.lower().split()
+        for word in words:
+            if word not in stop_words:
+                count[word] = count.get(word, 0) +1 
+                
+                
+            
+    max=0
+    most_frequent_word= None
+    for word in count:
+        if count[word]>max:
+            max= count[word]
+            most_frequent_word= word 
+            
+    return most_frequent_word
+
+sentences = [
+    "AI is the future",
+    "The future is bright",
+    "AI helps humans",
+    "Humans and AI together"
+]
+
+stop_words = ["is", "the", "and"]
+
+print(frequent_Words(sentences, stop_words))
