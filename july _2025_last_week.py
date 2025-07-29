3940a137d46cced4422ab71fbba114c1a722345a
Dixith <dixith400@gmail.com>
2025-07-29 18:33:58 +0530

    Update july _2025_last_week.py

--- july _2025_last_week.py
+++ july _2025_last_week.py
@@ -14,4 +14,18 @@ def words_length_grouping(words):
         
 words= ["ai", "code", "brain", "xai", "go", "think", "do"]        
 print(words_length_grouping(words))
-       
+
+
+# to check how many numbers of the same alphabet or other characters are there in the string 
+s = "The AI and the ai are great"
+def word(str):
+    letters_count={}
+    lower_case= str.lower()
+    for letter in lower_case:
+        if letter != " ":
+            letters_count[letter]=letters_count.get(letter,0)+1
+         
+         
+    return letters_count   
+        
+        
